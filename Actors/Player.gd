dac4617f910788c34f0a518d368c5b1ecb0ced84
matos <18725330+YoupiMatos@users.noreply.github.com>
2023-11-19 11:12:06 +0100

    Adds pickup and throw prototype

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -5,6 +5,13 @@ extends CharacterBody3D
 @export var JUMP_VELOCITY = 4.5
 @export var facing = 12
 
+const THROW_FORCE = 2
+
+var current_liftable_item: RigidBody3D
+var lifted_item: RigidBody3D
+var lifting_item: bool = false
+var throw_cooldown: float
+
 func _ready():
 	pass
 	# $AnimationPlayer.play("movement")
@@ -13,14 +20,10 @@ func _ready():
 var gravity = 8
 
 func _physics_process(delta):
-	gravity = 8# ProjectSettings.get_setting("physics/3d/default_gravity")
+	gravity = 8 # ProjectSettings.get_setting("physics/3d/default_gravity")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,7 +43,9 @@ func _physics_process(delta):
 #	else:
 #		$AnimationPlayer.play("idle")
 #	update_animation()
-	
+
+	item_pickup()
+	item_throw()
 	move_and_slide()
 	
 	
@@ -80,5 +85,35 @@ func update_facing(input_dir):
 	if input_dir.x < 0 and input_dir.y < 0:
 		facing = 15
 
+func item_pickup():
+	if current_liftable_item != null && Input.is_action_just_pressed("interact"):
+		lifted_item = current_liftable_item
+		print("trying to pick up")
+		lifted_item.freeze = true
+		lifting_item = true
+		throw_cooldown = 0.2
+		
+	if lifting_item:
+		lifted_item.global_position = global_position + Vector3(0, 0.5, 0)
+
+func item_throw():
+	if lifted_item != null && Input.is_action_just_pressed("interact"):
+		if throw_cooldown <= 0:
+			print("throwing!")
+			lifted_item.freeze = false
+			if velocity != Vector3.ZERO:
+				lifted_item.apply_impulse(velocity * THROW_FORCE)
+			else: lifted_item.apply_impulse(Vector3(0, 0, 1))
+			lifted_item = null
+			lifting_item = false
+	elif throw_cooldown > 0:
+		throw_cooldown -= get_process_delta_time()
+
+func _on_pickup_range_body_entered(body):
+	if body.is_in_group("liftable") && body is RigidBody3D:
+		current_liftable_item = body
 
 
+func _on_pickup_range_body_exited(body):
+	if body == current_liftable_item:
+		current_liftable_item = null
